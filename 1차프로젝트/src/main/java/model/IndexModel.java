2604a2871745b6ceaaf186c4a7e837aaf43c0163
heeJNa <jim2353@naver.com>
2022-02-18 21:14:34 +0900

    MVC구조로 변경

---
+++ 1차프로젝트/src/main/java/model/IndexModel.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.RequestMapping;
+import dao.BookDAO;
+import vo.Book;
+
+public class IndexModel extends HttpServlet {
+
+    @RequestMapping("main/main.do")
+    public String index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext application = request.getServletContext();
+
+        BookDAO bookDAO = new BookDAO();
+        List<Book> bestBooks;
+        List<Book> newBooks;
+        List<Book> youtubeBooks;
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        String sortBy = application.getInitParameter("DEFAULT_SORT");
+        System.out.println(sortBy);
+        // 베스트 셀러 & 신간 도서 비즈니스 로직 처리
+        map.put("sortBy", sortBy);
+        map.put("start", 1);
+        map.put("end", 12);
+
+        bestBooks = bookDAO.selectList(map);
+        map.replace("sortBy", "regdate");
+        newBooks = bookDAO.selectList(map);
+        youtubeBooks = bookDAO.selectYoutubeList();
+        bookDAO.close();
+
+        request.setAttribute("bestBooks", bestBooks);
+        request.setAttribute("newBooks", newBooks);
+        request.setAttribute("youtubeBooks", youtubeBooks);
+        request.setAttribute("main_jsp", "../main/Common.jsp");
+
+
+        return "../main/main.jsp";
+    }
+}
+
--- 1차프로젝트/src/main/webapp/js/index.js
+++ 1차프로젝트/src/main/webapp/js/index.js
@@ -1,69 +1,164 @@
-showSlides();
-function showSlides(n = 1) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var descriptions = document.getElementsByClassName("myDescription");
-  var dots = document.getElementsByClassName("demo");
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// ================ 네비게이션 바 파트 ======================
+// Mobile 네비게이션
+function toggleMobileMainList() {
+  var mobMainlist = document.getElementById("mobile-mainlist"),
+  mobDomesticlist = document.getElementById("mobile-domesticlist"),
+  mobForeignlist = document.getElementById("mobile-foreignlist"),
+  mobEbooklist = document.getElementById("mobile-ebooklist");
+  if (mobMainlist.style.width === "50%") {
+      if (mobDomesticlist.style.width === "50%" || mobForeignlist.style.width === "50%" || mobEbooklist.style.width === "50%") {
+        mobDomesticlist.style.width = '0%'; mobForeignlist.style.width = '0%'; mobEbooklist.style.width = '0%';
+        setTimeout(function(){
+          mobMainlist.style.fontSize = '0px';
+          mobMainlist.style.width = '0%';
+        }, 100)
+      } else {
+        mobMainlist.style.fontSize = '0px';
+        mobMainlist.style.width = '0%';
+      }
+  } else {
+    mobMainlist.style.width = '50%';
+    mobMainlist.style.fontSize = '30px';
   }
-  for (i = 0; i < descriptions.length; i++) {
-      descriptions[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].style.opacity = "0.6";
-  }
-  slides[n-1].style.display = "block";
-  descriptions[n-1].style.display = "block";
-  dots[n-1].style.opacity = "1";
 }
-// ================== 메인페이지 파트 ==========================
-const swiper = new Swiper('.book-swiper', {
-  autoHeight: true,
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 50,
-  autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 120
-    },
-    // when window width is >= 480px
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 50
-    },
-    // when window width is >= 640px
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 40
+function toggleMobileDomesticList() {
+  var mobDomesticlist = document.getElementById("mobile-domesticlist"),
+  mobForeignlist = document.getElementById("mobile-foreignlist"),
+  mobEbooklist = document.getElementById("mobile-ebooklist");
+  if (mobDomesticlist.style.width = "0%") {
+    if (mobForeignlist.style.width === "50%" || mobEbooklist.style.width === "50%") {
+      mobForeignlist.style.width = '0%';
+      mobEbooklist.style.width = '0%';
+      setTimeout(function(){
+          mobDomesticlist.style.width = '50%';
+        }, 100)
+    } else {
+      mobDomesticlist.style.width = '50%';
     }
+  } else {
+    mobDomesticlist.style.width = '0%';
   }
+}
+function toggleMobileForeignList() {
+  var mobDomesticlist = document.getElementById("mobile-domesticlist"),
+  mobForeignlist = document.getElementById("mobile-foreignlist"),
+  mobEbooklist = document.getElementById("mobile-ebooklist");
+  if (mobForeignlist.style.width = "0%") {
+    if (mobDomesticlist.style.width === "50%" || mobEbooklist.style.width === "50%") {
+      mobDomesticlist.style.width = '0%';
+      mobEbooklist.style.width = '0%';
+      setTimeout(function(){
+          mobForeignlist.style.width = '50%';
+        }, 100)
+    } else {
+      mobForeignlist.style.width = '50%';
+    }
+  } else {
+    mobForeignlist.style.width = '0%';
+  }
+}
+function toggleMobileEbookList() {
+  var mobDomesticlist = document.getElementById("mobile-domesticlist"),
+  mobForeignlist = document.getElementById("mobile-foreignlist"),
+  mobEbooklist = document.getElementById("mobile-ebooklist");
+  if (mobEbooklist.style.width = "0%") {
+    if (mobForeignlist.style.width === "50%" || mobDomesticlist.style.width === "50%") {
+      mobDomesticlist.style.width = '0%';
+      mobForeignlist.style.width = '0%';
+      setTimeout(function(){
+          mobEbooklist.style.width = '50%';
+        }, 100)
+    } else {
+      mobEbooklist.style.width = '50%';
+    }
+  } else {
+    mobEbooklist.style.width = '0%';
+  }
+}
+// PC 네비게이션
+function toggleMainList() {
+  var navlistarea = document.getElementById("navListArea"),
+  mainlist = document.getElementById("mainList"),
+  domesticlist = document.getElementById("domesticList"),
+  foreignlist = document.getElementById("foreignList"),
+  ebooklist = document.getElementById("ebookList");
+  if (mainlist.style.height === "100%") {
+      if (domesticlist.style.width === "20%" || foreignlist.style.width === "20%" || ebooklist.style.width === "20%") {
+      	domesticlist.style.width = '0%'; foreignlist.style.width = '0%'; ebooklist.style.width = '0%';
+      	setTimeout(function(){
+          mainlist.style.height = '0%';
+          navlistarea.style.height = '0%';
+        }, 100)
+      } else {
+      	mainlist.style.height = '0%';
+        navlistarea.style.height = '0%';
+      }
+  } else {
+    navlistarea.style.height = '76vh';
+    mainlist.style.height = '100%';
+  }
+}
+function toggleDomesticList() {
+  var domesticlist = document.getElementById("domesticList"),
+  foreignlist = document.getElementById("foreignList"),
+  ebooklist = document.getElementById("ebookList");
+  if (domesticlist.style.width = "0%") {
+  	if (foreignlist.style.width === "20%" || ebooklist.style.width === "20%") {
+  		foreignlist.style.width = '0%';
+  		ebooklist.style.width = '0%';
+  		setTimeout(function(){
+          domesticlist.style.width = '20%';
+        }, 100)
+  	} else {
+  		domesticlist.style.width = '20%';
+  	}
+  } else {
+  	domesticlist.style.width = '0%';
+  }
+}
+function toggleForeignList() {
+  var domesticlist = document.getElementById("domesticList"),
+  foreignlist = document.getElementById("foreignList"),
+  ebooklist = document.getElementById("ebookList");
+  if (foreignlist.style.width = "0%") {
+  	if (domesticlist.style.width === "20%" || ebooklist.style.width === "20%") {
+  		domesticlist.style.width = '0%';
+  		ebooklist.style.width = '0%';
+  		setTimeout(function(){
+          foreignlist.style.width = '20%';
+        }, 100)
+  	} else {
+  		foreignlist.style.width = '20%';
+  	}
+  } else {
+  	foreignlist.style.width = '0%';
+  }
+}
+function toggleEbookList() {
+  var domesticlist = document.getElementById("domesticList"),
+  foreignlist = document.getElementById("foreignList"),
+  ebooklist = document.getElementById("ebookList");
+  if (ebooklist.style.width = "0%") {
+  	if (foreignlist.style.width === "20%" || domesticlist.style.width === "20%") {
+  		domesticlist.style.width = '0%';
+  		foreignlist.style.width = '0%';
+  		setTimeout(function(){
+          ebooklist.style.width = '20%';
+        }, 100)
+  	} else {
+  		ebooklist.style.width = '20%';
+  	}
+  } else {
+  	ebooklist.style.width = '0%';
+  }
+}
+var navSwiper = new Swiper(".navSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
 });
-const eventSwiper = new Swiper(".eventSwiper", {
-	autoHeight: true,
-    spaceBetween: 30,
-    effect: "fade",
-    loop: true,
-    autoplay: {
-      delay: 10000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
